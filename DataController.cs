e2cb9f899aa8e33f8a5cc4716bd77dfa9691e3dc
JHurst87 <72364767+JHurst87@users.noreply.github.com>
2020-10-15 09:37:19 -0500

    Add files via upload

---
+++ DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace DataController
+{
+    class DataControl
+    {
+        readonly string path = @"C:\Users\thepa\Desktop\Internship\WPF\PersonInfoTXT\Text.txt";
+
+        public void ToDatabase(string firstName, string lastName)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            string fullName = firstName + " " + lastName;
+            File.AppendAllText(path, fullName + Environment.NewLine);            
+        }
+
+        //public void DeleteInfo()
+        //{
+        //    File.Delete(path);
+        //}
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using DataController;
+using System.IO;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        readonly DataControl data = new DataControl();
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstName.Text;
+            string lastName = LastName.Text;
+            data.ToDatabase(firstName, lastName);
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            using StreamReader sr = new StreamReader(@"C:\Users\thepa\Desktop\Internship\WPF\PersonInfoTXT\Text.txt");            
+            Results.Text = sr.ReadToEnd();
+        }
+
+        //private void DeleteAll_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+    }
+}
